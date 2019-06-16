76243259c1eacf725151cfd772db6eb8accdffb1
Thomas Lisankie <thomas@tomlisankie.com>
2019-06-16 13:48:50 -0400

    Added piano functions to index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("loading image");
         let reader = new FileReader();
         reader.onload = function(event) {
-            resetSlider();
             currentCanvas.replaceImage(event.target.result, context);
             currentCanvas.draw(context);
         };
@@ -38,6 +37,39 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         currentCanvas.draw(context);
     }
+
+    const keys = document.querySelectorAll(".key"),
+            note = document.querySelector(".nowplaying"),
+            hints = document.querySelectorAll(".hints");
+
+    function playNote(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
+        key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+        if (!key) return;
+
+        const keyNote = key.getAttribute("data-note");
+        
+        key.classList.add("playing");
+        note.innerHTML = keyNote;
+        audio.currentTime = 0;
+        audio.play();
+    }
+
+    function removeTransition(e) {
+        if (e.propertyName !== "transform") return;
+        this.classList.remove("playing");
+    }
+      
+    function hintsOn(e, index) {
+        e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
+    }
+    
+    hints.forEach(hintsOn);
+    
+    keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+    
+    window.addEventListener("keydown", playNote);
 
     function generatePoints(pointCount) {
         for(let i = 0; i < pointCount; i++){
